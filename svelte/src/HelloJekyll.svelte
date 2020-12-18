f176a90ace1ca3625f64a58221169ebbd333dae6
sharath <sharath.dt@gmail.com>
2020-12-18 07:35:42 +0530

    udpate styles

--- svelte/src/HelloJekyll.svelte
+++ svelte/src/HelloJekyll.svelte
@@ -11,6 +11,7 @@
 		padding: 2rem 1rem;
 		margin-bottom: 3rem;
 		border: 1px solid #ddd;
+		border-radius: 0.25rem;
 		box-shadow: 0 10px 15px -3px rgba(0,0,0,0.1),0 4px 6px -2px rgba(0,0,0,0.05);
 		background-color: white;
 	}
--- svelte/src/Sample.svelte
+++ svelte/src/Sample.svelte
@@ -7,6 +7,7 @@
     padding: 2rem 1rem;
     border: 1px solid #ddd;
     margin-bottom: 3rem;
+    border-radius: 0.25rem;
     box-shadow: 0 10px 15px -3px rgba(0,0,0,0.1),0 4px 6px -2px rgba(0,0,0,0.05);
     background-color: white;
   }
